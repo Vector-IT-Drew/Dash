082c6d4e278c0b6b766132ec2f6433f5a0a0ba90
drew wood <drewwood@drews-MBP.localdomain>
2025-04-21 17:41:01 -0400

    Update

--- Services/Forms/Get_Gmail_Timeslots.py
+++ Services/Forms/Get_Gmail_Timeslots.py
@@ -78,6 +78,7 @@ def get_gmail_service(email_address):
     print('get_gmail_service', email_address)
     
     # Use environment variables instead of service account file
+    print('Preloaded:', os.environ.get('GOOGLE_CREDS', '{}'))
     service_account_info = json.loads(os.environ.get('GOOGLE_CREDS', '{}'))
     print('service_account_info', service_account_info)
 
