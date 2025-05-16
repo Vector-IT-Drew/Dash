2bd6d0219369ffd9ce822af5f002dd1477e55e2b
drew wood <drewwood@drews-MBP.localdomain>
2025-05-16 16:30:06 -0400

    Initial commit

--- Services/Database/Listings.py
+++ Services/Database/Listings.py
@@ -174,6 +174,7 @@ def get_filtered_listings_data(
                         )
                     )
                     {proximity_filter}
+                """
         except Exception as e:
             raise
 
