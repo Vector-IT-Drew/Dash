18705e05bc38d110b52da0d28069e7f0048bd955
drew wood <drewwood@drews-MBP.localdomain>
2025-05-22 12:18:59 -0400

    Initial commit

--- Services/Database/Data.py
+++ Services/Database/Data.py
@@ -324,7 +324,8 @@ queries = {
         FROM notes n
         LEFT JOIN units u ON n.target_type = 'units' AND n.target_id = u.unit_id
         LEFT JOIN addresses a ON u.address_id = a.address_id
-        LEFT JOIN persons p ON n.creator_id = p.person_id
+        LEFT JOIN persons person ON n.creator_id = person.person_id
+        LEFT JOIN portfolios p ON a.entity_id = p.entity_id
         WHERE n.target_type = %s AND n.target_id = %s
     """,
     'get_unit_deals': """
@@ -339,7 +340,11 @@ queries = {
             actual_rent,
             term,
             concession
-        FROM deals
+        FROM deals d
+        LEFT JOIN units u ON d.unit_id = u.unit_id
+        LEFT JOIN addresses a ON u.address_id = a.address_id
+        LEFT JOIN entities e ON a.entity_id = e.entity_id
+        LEFT JOIN portfolios p ON e.portfolio_id = p.portfolio_id
         WHERE unit_id = %s
     """
 }
