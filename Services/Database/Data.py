52e04c6afe92d198f6c36f1151f6593e799156b8
drew wood <drewwood@drews-MBP.localdomain>
2025-05-22 12:27:09 -0400

    Initial commit

--- Services/Database/Data.py
+++ Services/Database/Data.py
@@ -320,7 +320,10 @@ queries = {
         WHERE 1=1
     """,
     'get_notes': """
-        SELECT n.*, a.address, p.first_name AS first_name, p.last_name AS last_name, p.person_id
+        SELECT n.*, a.address, 
+            person.first_name AS first_name, 
+            person.last_name AS last_name, 
+            person.person_id
         FROM notes n
         LEFT JOIN units u ON n.target_type = 'units' AND n.target_id = u.unit_id
         LEFT JOIN addresses a ON u.address_id = a.address_id
